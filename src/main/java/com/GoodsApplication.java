82e1eac2296c95cfea78ffc524b60a820ac64632
kingian <36391369+qinjiangLove@users.noreply.github.com>
2019-03-14 15:23:51 +0800

    war

--- src/main/java/com/GoodsApplication.java
+++ src/main/java/com/GoodsApplication.java
@@ -3,15 +3,22 @@ package com;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 //@EnableEurekaClient
 @MapperScan("com.**.dao")
 @SpringBootApplication
-public class GoodsApplication {
+public class GoodsApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(GoodsApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(GoodsApplication.class);
     }
 
 }
