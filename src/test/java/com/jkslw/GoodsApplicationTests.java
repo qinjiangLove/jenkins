82c76f08cca2706244d9a8f58c182c67d18e4bfd
kingian <36391369+qinjiangLove@users.noreply.github.com>
2019-03-18 10:38:14 +0800

    git commit test in idea

--- src/test/java/com/jkslw/GoodsApplicationTests.java
+++ src/test/java/com/jkslw/GoodsApplicationTests.java
@@ -1,17 +1,17 @@
-package com.jkslw;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class GoodsApplicationTests {
-
-    @Test
-    public void contextLoads() {
-    }
-
-}
-
+//package com.jkslw;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class GoodsApplicationTests {
+//
+//    @Test
+//    public void contextLoads() {
+//    }
+//
+//}
+//
